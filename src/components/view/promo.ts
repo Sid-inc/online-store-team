e680ea64178d251e71ac2f8cfd7aa923f6618b4b
Szyrwel <potato_father@list.ru>
2023-02-07 07:58:42 +0300

    fix: delete link Shop now

--- src/components/view/promo.ts
+++ src/components/view/promo.ts
@@ -3,14 +3,7 @@ import { createNode } from '../utils/createNode';
 export class Promo {
   draw() {
     const promo = createNode({ tag: 'div', classes: ['promo'] });
-    const promoContainer = createNode({ tag: 'div', classes: ['promo__inner', 'container'], parent: promo });
-    createNode({
-      tag: 'a',
-      classes: ['promo__link'],
-      atributesAndValues: [['href', '#shop']],
-      text: 'Shop now',
-      parent: promoContainer,
-    });
+    createNode({ tag: 'div', classes: ['promo__inner', 'container'], parent: promo });
     document.body.append(promo);
   }
 }
